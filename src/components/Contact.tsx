e81e9650d1974b4d24b3289e6338bf914ae3a23b
YASH RASTOGI <rastogi21yr@gmail.com>
2026-09-24 19:07:32 +0530

    Update GitHub link in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -24,7 +24,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com/yash21092005"
+              href="https://github.com/gravityash"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
